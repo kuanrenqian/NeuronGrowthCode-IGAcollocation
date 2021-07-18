9986d9d6992c2f3b764be89245aa05e0e5a1185a
kuanrenqian <44757871+kuanrenqian@users.noreply.github.com>
2021-07-17 21:41:22 -0400

    Modified sparse matrix setup
    Modified how sparse matrix is created, should help with memory issue to some extent

--- IGA_collocation_algorithm/initialize_neurite_growth.m
+++ IGA_collocation_algorithm/initialize_neurite_growth.m
@@ -1,20 +1,34 @@
-function [phi, conc, conct] = initialize_neurite_growth(seed_radius, lenu, lenv, dx)
-phi = zeros([lenu,lenv]); % with ghost nodes (technically Nx by Ny, but becomes size_collpts after adding ghost points)
-conc = 0.75.*ones([lenu,lenv]);
-conct = zeros(lenu,lenv);
-seed = (seed_radius*dx)^2;
+function [phi, conc] = initialize_neurite_growth(seed_radius, lenu, lenv)
+
+seed = (seed_radius)^2;
+
+ind_i = [];
+ind_j = [];
+
+phi_val = [];
+
 for i=1:lenu
     for j=1:lenv
         if ((i-lenu/2)*(i-lenu/2)+(j-lenv/2)*(j-lenv/2) < seed)
-            r = sqrt((i-lenu/2)*(i-lenu/2)+(j-lenv/2)*(j-lenv/2));
-%             phi(i+15,j+15) = 1.0;
-%             phi(i-15,j-15) = 1.0;
-%             conc(i+15,j+15) = 0.5;
-%             conc(i-15,j-15) = 0.5;
             
-            phi(i,j) = 1.0;
-            conc(i,j) = 0.5;
-            conct(i,j) = (0.5+0.5*tanh((sqrt(seed)-r)/2));
+            % single neuron
+            ind_i(end+1) = i;
+            ind_j(end+1) = j;
+            phi_val(end+1) = 1;            
+
+            % multi neuron
+%             ind_i(end+1) = i+15;
+%             ind_i(end+1) = i-15;
+%             ind_j(end+1) = j+15;
+%             ind_j(end+1) = j-15;
+%             
+%             phi_val(end+1) = 1;            
+%             phi_val(end+1) = 1;                    
+            
         end
     end
 end
+
+
+phi = sparse(ind_i,ind_j,phi_val,lenu,lenv);
+conc = phi*0.5; % conc just happens to be 0.5, this is faster
